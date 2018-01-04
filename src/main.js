28485d0e9aac0fb89fa5dd428ac9eec38579698b
Jerome1580 <raoju1580@qq.com>
2018-01-05 00:26:09 +0800

    初始化music-vue项目

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// body 下面div的点击没有300ms的延迟
+import fastclick from 'fastclick'
+fastclick.attach(document.body);
+
+Vue.config.productionTip = false
+
+import 'common/stylus/index.styl'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
