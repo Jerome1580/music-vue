948b728ce24569109484ae286a3f155ae266eb04
raoju <raoju1580@qq.com>
2018-01-07 13:25:02 +0800

    渲染推荐歌单；封装scroll基础组件

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,25 +1,37 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <div v-if="recommends.length" class="slider-wrapper">
-        <Slider>
-          <div v-for="item in recommends">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl">
-            </a>
-          </div>
-        </Slider>
+    <Scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper">
+          <Slider>
+            <div v-for="item in recommends">
+              <a :href="item.linkUrl">
+                <img @load="loadImage" :src="item.picUrl">
+              </a>
+            </div>
+          </Slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" class="item">
+              <div class="icon">
+                <img width="60" height="60" :src="item.imgurl">
+              </div>
+              <div class="text">
+                <h2 class="name" v-for="item2 in item" v-if="item2.name" v-html="item2.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-        </ul>
-      </div>
-    </div>
+    </Scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
   import Slider from 'base/slider/slider'
   import {getRecommend} from 'api/recommend'
   import {ERR_OK} from 'api/config'
@@ -27,11 +39,13 @@
   export default {
     data(){
       return {
-        recommends: []
+        recommends: [],
+        discList: []
       }
     },
     created(){
-      this._getRecommend()
+      this._getRecommend();
+      this._getDiscList();
     },
     methods: {
       _getRecommend(){
@@ -41,10 +55,23 @@
           }
 
         })
+      },
+      _getDiscList(){
+        // 暂时用mock数据
+        this.$axios.get('static/mock/DiscList.json').then((res) => {
+          this.discList = res.data;
+        })
+      },
+      loadImage(){
+        if (!this.checkLoaded) {
+          this.$refs.scroll.refresh();
+          this.checkLoaded = true;
+        }
       }
     },
     components: {
-      Slider
+      Slider,
+      Scroll
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
 // body 下面div的点击没有300ms的延迟
 import fastclick from 'fastclick'
 fastclick.attach(document.body);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
 
 import 'common/stylus/index.styl'
 
