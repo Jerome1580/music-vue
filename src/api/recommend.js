866c94fdc9d426257df2ae39e457f8d7596750df
Jerome1580 <raoju1580@qq.com>
2018-01-08 22:43:53 +0800

    新增Singer-detail组件及路由初始化

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,7 +1,7 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 
-
+// 获取轮播海报数据
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg';
 
@@ -10,6 +10,25 @@ export function getRecommend() {
     uin: 0,
     needNewCode: 1
   });
+
+  return jsonp(url, data, options)
+}
+
+
+export function getDiscList() {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg';
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'yqq',
+    hostUin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    needNewCode: 0,
+    categoryId: 10000000,
+    rnd: Math.random(),
+    format: 'json'
+  })
 
   return jsonp(url, data, options)
 }
