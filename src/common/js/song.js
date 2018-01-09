1e3cf4726178e30147ecb7908553c718a06a201f
Jerome1580 <raoju1580@qq.com>
2018-01-10 02:23:02 +0800

    封装歌曲Song类，（工厂函数返回），处理歌曲数据

---
+++ src/common/js/song.js
@@ -0,0 +1,37 @@
+export default class Song {
+  constructor({id, mid, singer, name, album, duration, image, url}) {
+    this.id = id
+    this.mid = mid
+    this.singer = singer
+    this.name = name
+    this.album = album
+    this.duration = duration
+    this.image = image
+    this.url = url
+  }
+}
+
+export function createSong(musicData) {
+  return new Song({
+    id: musicData.latest_song.songid,
+    mid: musicData.albumMID,
+    singer: filterSinger(musicData.singers),
+    name: musicData.latest_song.track_name,
+    album: musicData.albumName,
+    duration: musicData.listen_count,
+    image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albumMID}.jpg?max_age=2592000`,
+    url: `https://ws.stream.qqmusic.qq.com/${musicData.latest_song.songid}.m4a?fromtag=46`
+  })
+}
+
+function filterSinger(singer) {
+  let ret = []
+  if (!singer) {
+    return ''
+  }
+  singer.forEach((s) => {
+    "use strict";
+    ret.push(s.singer_name)
+  })
+  return ret.join('/')
+}
